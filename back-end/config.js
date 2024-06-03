a70a3eee4143477e84d025fa752e0cae42b2a2e7
saneem-bk <150452629+saneem-bk@users.noreply.github.com>
2024-06-03 16:27:43 +0530

    Update config.js

--- back-end/config.js
+++ back-end/config.js
@@ -1,2 +1,2 @@
-PORT = 4001;
-MONGODBurl = "mongodb+srv://bksaneem:YnERG9RZdhx9cQzV@bookstore.flzllvd.mongodb.net/?retryWrites=true&w=majority&appName=BookStore";
+export const PORT = 4001;
+export const MONGODBurl = "mongodb+srv://bksaneem:YnERG9RZdhx9cQzV@bookstore.flzllvd.mongodb.net/?retryWrites=true&w=majority&appName=BookStore";
