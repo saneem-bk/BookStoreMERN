8cdeb489eb28d062a71d95ad98d47226abcd828b
saneem-bk <bksaneem@gmail.com>
2024-06-03 16:08:24 +0530

    Update config.js

--- back-end/config.js
+++ back-end/config.js
@@ -1,2 +1,2 @@
-PORT = 4001 
-MONGODBurl = "mongodb+srv://bksaneem:YnERG9RZdhx9cQzV@bookstore.flzllvd.mongodb.net/?retryWrites=true&w=majority&appName=BookStore"
+PORT = 4001;
+MONGODBurl = "mongodb+srv://bksaneem:YnERG9RZdhx9cQzV@bookstore.flzllvd.mongodb.net/?retryWrites=true&w=majority&appName=BookStore";
